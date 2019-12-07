9e28d8de1e9c1758864c70844721b2641e2f2cf4
remoo1901 <remoo1901@hotmail.com>
2019-12-06 23:43:27 -0500

    MVP completed

--- js/index.js
+++ js/index.js
@@ -53,3 +53,98 @@ desImg.addEventListener('mouseenter', (el) =>{
 desImg.addEventListener('mouseleave', (el) =>{
     el.target.style.transform = ('rotate(0.0turn)')
 })
+
+// navLinks
+
+let navLinks = document.querySelector(".nav, .nav-link");
+
+
+
+navLinks.addEventListener('mouseup', x => {
+    x.target.style.color = "red ";
+    x.target.style.transitionDuration = "2s";
+})
+
+
+let navLink = document.querySelector(".nav, .nav-link");
+
+navLink.addEventListener('click', (event) => {
+    event.preventDefault()
+})
+
+// h2
+
+let h2d = document.querySelector('h2');
+
+h2d.addEventListener('dblclick', () => {
+    alert("Welcome to Fun Bus!  :)");
+})
+
+
+// bus Sound
+
+const sound = document.querySelector('.busSound');
+
+const busImg = document.querySelector('[src="img/fun-bus.jpg"]');
+
+busImg.addEventListener('dblclick', (x) => {
+    sound.play();
+})
+
+// footer 
+
+const ftr = document.querySelector('.footer');
+const crt = document.querySelector('.copyright');
+
+ftr.addEventListener('click', (x)=> {
+    x.target.style.color = 'blue';
+})
+
+crt.addEventListener('click', (y) => {
+   y.target.style.color ='red';
+   y.stopPropagation()
+})
+
+
+const btn = document.querySelector('#btn0');
+
+btn.addEventListener('mouseenter', (el) => {
+    el.target.style.transform = ("rotate(2deg) translateX(30px)");
+    el.target.style.transitionDuration = "2s";
+    el.target.style.color = "red";
+})
+    
+btn.addEventListener('mouseleave', (el) => {
+    el.target.style.transform = ("rotate(0deg) translateX(-10px)");
+    el.target.style.transitionDuration = "1.5s";
+    el.target.style.color = "white ";
+})
+
+const btN1 = document.querySelector('#btn1');
+
+ btN1.addEventListener('mouseenter', (eil) => {
+    eil.target.style.transform = ("rotate(2deg) translateX(30px)");
+    eil.target.style.transitionDuration = "2s";
+    eil.target.style.color = "red";
+})
+
+btN1.addEventListener('mouseleave', (eil) => {
+    eil.target.style.transform = ("rotate(0deg) translateX(-10px)");
+    eil.target.style.transitionDuration = "1.5s";
+    eil.target.style.color = "white ";
+})
+
+
+const btN2 = document.querySelector('#btn2');
+
+ btN2.addEventListener('mouseenter', (z) => {
+    z.target.style.transform = ("rotate(2deg) translateX(30px)");
+    z.target.style.transitionDuration = "2s";
+    z.target.style.color = "red";
+})
+
+btN2.addEventListener('mouseleave', (z) => {
+    z.target.style.transform = ("rotate(0deg) translateX(-10px)");
+    z.target.style.transitionDuration = "1.5s";
+    z.target.style.color = "white ";
+})
